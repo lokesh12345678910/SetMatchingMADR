4c542cb41509950549eb411c7bf901bc4d3125bc
Stephanie Grasso <smgrasso1@login1.ls6.tacc.utexas.edu>
2023-08-24 02:26:46 -0500

    looping implemented

---
+++ setMatchingChangedWhileLoop(NF).py
@@ -0,0 +1,371 @@
+"""#Changed spreadsheet
+
+##load in changed spreadsheet
+"""
+
+import sys
+inputSpreadSheet = sys.argv[1]#'changedGeneratedSetsWithFeats.csv''
+outputDirectory = 'changed_' + sys.argv[2]
+assert outputDirectory[-1] == '/', print("output directory must end with /")
+p_condition = float(sys.argv[3])
+#make outputDirectory if it doesn't exist
+import os
+if not os.path.exists(outputDirectory):
+    os.makedirs(outputDirectory)
+    print(f"Directory '{outputDirectory}' created.")
+
+
+import pandas as pd
+import numpy as np
+
+changedSetsWithFeatsDF = pd.read_csv(inputSpreadSheet)
+changedSetsWithFeatsDF.head()
+
+"""##Fix assignment order"""
+
+originalAssignmentOrder = list(changedSetsWithFeatsDF['Assignment'])
+
+del changedSetsWithFeatsDF['Assignment']
+changedSetsWithFeatsDF.head()
+
+#sorted by Set #
+changedSetsWithFeatsDF = changedSetsWithFeatsDF.sort_values('Set #')
+changedSetsWithFeatsDF.head()
+
+#bring bank assignment column
+changedSetsWithFeatsDF.insert(0, "Assignment", originalAssignmentOrder)
+
+changedSetsWithFeatsDF
+
+"""##split into 5"""
+
+splitGeneratedSetsDf = np.array_split(changedSetsWithFeatsDF, 8) # THIS LINE IS VERY IMPORANT, changedSetsWithFeatsDF instead of generatedSetsDF
+len(splitGeneratedSetsDf)
+
+
+"""#t-tests
+
+##Within Spanish
+"""
+
+from scipy import stats
+
+descriptors = []
+for i in range(8):
+  for j in range(8):
+    if i == j:
+      continue
+    descriptors.append("S" + str(i+1) + "," + str(j+1))
+
+def ttest_feature(feature):
+  p_vals = []
+  for i in range(8):
+    for j in range(8):
+      if i == j:
+        #p_vals.append(np.nan)
+        continue
+      list1 = splitGeneratedSetsDf[i][feature].dropna()
+      list2 = splitGeneratedSetsDf[j][feature].dropna()
+      if len(list1) <= 2:
+        p_vals.append(np.nan)
+        continue
+      if len(list2) <= 2:
+        p_vals.append(np.nan)
+        continue
+      _, p_value = stats.ttest_ind(list1, list2)
+      if p_value < p_condition:
+        print("Within Spanish, for the feature", feature, "p value between set", str(i+1), "and", str(j+1), "is <=", str(p_condition))
+        exit(1)
+      p_vals.append(p_value)
+  return p_vals
+
+ttest_feature('Espal_CON')
+
+ttest_feature('Espal_NSYL')
+
+len(ttest_feature("Espal_NLET"))
+
+cols = ['Espal_NSYL','Espal_NLET','ClearpondFrequency_PerMil_Esp',"Coca_word Freq (Total) - Web/Dialects"]
+
+ttestData = np.array([ttest_feature(col) for col in cols])
+
+ttestDF = pd.DataFrame(ttestData.T, columns = cols)
+ttestDF.head()
+
+ttestDF.insert(0, "Ttests Within Languages_Spanish", descriptors)
+
+ttestDF.head()
+
+ttestDF.tail()
+
+ttestDF.fillna('Invalid',inplace=True)
+
+ttestDF.to_csv(outputDirectory  + 'ttestWithinSpanish.csv', index=False)
+
+"""##Within Catalan"""
+
+
+def ttest_feature(feature):
+  p_vals = []
+  for i in range(8):
+    for j in range(8):
+      if i == j:
+        #p_vals.append(np.nan)
+        continue
+      list1 = splitGeneratedSetsDf[i][feature].dropna()
+      list2 = splitGeneratedSetsDf[j][feature].dropna()
+      if len(list1) <= 2:
+        p_vals.append(np.nan)
+        continue
+      if len(list2) <= 2:
+        p_vals.append(np.nan)
+        continue
+      _, p_value = stats.ttest_ind(list1, list2)
+      if p_value < p_condition:
+        print("Within Catalan, for the feature", feature, "p value between set", str(i+1), "and", str(j+1), "is <=", str(p_condition))
+        exit(1)
+      p_vals.append(p_value)
+  return p_vals
+
+ttest_feature('CAT_SUBTLEX_NSYL')
+
+ttest_feature('NIM_NLET')
+
+ttest_feature('NIM_WordFreq_Relativa')
+
+ttest_feature('NIM_WordFreq_Total_Absoluta')
+
+cols = ['CAT_SUBTLEX_NSYL','NIM_NLET','NIM_WordFreq_Relativa',"NIM_WordFreq_Total_Absoluta"]
+
+ttestData = np.array([ttest_feature(col) for col in cols])
+
+ttestDF = pd.DataFrame(ttestData.T, columns = cols)
+ttestDF.head()
+
+ttestDF.insert(0, "Ttests Within Languages_Catalan", descriptors)
+
+ttestDF.head()
+
+ttestDF.tail()
+
+#ttestDF.fillna('Invalid',inplace=True)
+
+ttestDF.to_csv(outputDirectory + 'ttestWithinCatalan.csv', index=False)
+
+"""##Within English"""
+
+
+
+def ttest_feature(feature):
+  p_vals = []
+  for i in range(8):
+    for j in range(8):
+      if i == j:
+        #p_vals.append(np.nan)
+        continue
+      list1 = splitGeneratedSetsDf[i][feature].dropna()
+      list2 = splitGeneratedSetsDf[j][feature].dropna()
+      if len(list1) <= 2:
+        p_vals.append(np.nan)
+        continue
+      if len(list2) <= 2:
+        p_vals.append(np.nan)
+        continue
+      _, p_value = stats.ttest_ind(list1, list2)
+      if p_value < p_condition:
+        print("Within English, for the feature", feature, "p value between set", str(i+1), "and", str(j+1), "is <=", str(p_condition))
+        exit(1)
+      p_vals.append(p_value)
+  return p_vals
+
+
+
+
+cols = ['MRC Imigability','MRC Concreteness',
+       'MRC Familiarity', 'MRC N_of_Letters',
+       'MRC N_of_Syllables','Clearpond_Lexical_Frequency_Eng']
+
+ttestData = np.array([ttest_feature(col) for col in cols])
+
+# Create the pandas DataFrame
+ttestDF = pd.DataFrame(ttestData.T, columns = ['MRC Imageability', 'MRC Concretness', 'MRC Familiarity', 'MRC N_of_Letters', 'MRC_Number of Syllables', 'Clearpond lexical frequency'])
+
+# print dataframe.
+ttestDF.head()
+
+ttestDF.insert(0, "Ttests Within Languages_English", descriptors)
+
+ttestDF.head()
+
+ttestDF.tail()
+
+#ttestDF.fillna('Invalid',inplace=True)
+
+ttestDF.to_csv(outputDirectory + 'ttestWithinEnglish.csv', index=False)
+
+"""##English vs Spanish"""
+
+def ttest_feature_across_lang(featureLang1, featureLang2):
+  p_vals = []
+  for i in range(8):
+    for j in range(8):
+      if i == j:
+        #p_vals.append(np.nan)
+        continue
+      list1 = splitGeneratedSetsDf[i][featureLang1].dropna()
+      list2 = splitGeneratedSetsDf[j][featureLang2].dropna()
+      if len(list1) <= 2:
+        p_vals.append(np.nan)
+        continue
+      if len(list2) <= 2:
+        p_vals.append(np.nan)
+        continue
+      _, p_value = stats.ttest_ind(list1, list2)
+      if p_value < p_condition:
+        print("Across English vs Spanish, for the feature pair", featureLang1, featureLang2, "p value between set", str(i+1), "and", str(j+1), "is <=", str(p_condition))
+        exit(1)
+      p_vals.append(p_value)
+  return p_vals
+
+ttest_feature_across_lang('Espal_NLET','MRC N_of_Letters')
+
+ttest_feature_across_lang('ClearpondFrequency_PerMil_Esp','Clearpond_Lexical_Frequency_Eng')
+
+ttest_feature_across_lang('Coca_word Freq (Total) - Web/Dialects','COCA_full freq Eng')
+
+
+
+ttestData = np.array([ttest_feature_across_lang('Espal_NLET','MRC N_of_Letters'), ttest_feature_across_lang('ClearpondFrequency_PerMil_Esp','Clearpond_Lexical_Frequency_Eng'),ttest_feature_across_lang('Coca_word Freq (Total) - Web/Dialects','COCA_full freq Eng')])
+
+# Create the pandas DataFrame
+ttestDF = pd.DataFrame(ttestData.T, columns = ['Number of letters', 'Clearpond_Frequency across languages', 'Freq_COCA'])
+
+# print dataframe.
+ttestDF.head()
+
+ttestDF.insert(0, "Ttests: Across Languages- Eng vs. Spanish", descriptors)
+
+ttestDF.head()
+
+ttestDF.tail()
+
+ttestDF.to_csv(outputDirectory  + 'ttestAcrossEnglishAndSpanish.csv', index=False)
+
+"""#Download generated sets with feats"""
+
+changedSetsWithFeatsDF.fillna('NaN', inplace=True)
+changedSetsWithFeatsDF.to_csv(outputDirectory  + 'changedSetsWithFeats.csv', index=False)
+
+"""# Assigning Part 2"""
+
+assignmentOrder = []
+
+for i in range(0,len(originalAssignmentOrder),5):
+  assignmentOrder.append(originalAssignmentOrder[i])
+
+
+trainedSpanishDFIndexes = [i for i, string in enumerate(assignmentOrder) if string == 'Trained in Spanish']
+trainedCatalanDFIndexes = [i for i, string in enumerate(assignmentOrder) if string == 'Trained in Catalan']
+untrainedIndexes = [i for i, string in enumerate(assignmentOrder) if string == 'Untrained']
+
+
+
+trainedSpanishDFs = [splitGeneratedSetsDf[i] for i in trainedSpanishDFIndexes]
+trainedCatalanDFs = [splitGeneratedSetsDf[i] for i in trainedCatalanDFIndexes]
+untrainedDFs = [splitGeneratedSetsDf[i] for i in untrainedIndexes]
+
+
+allTrainedSpanishDF = pd.concat(trainedSpanishDFs)
+allTrainedSpanishDF.head()
+
+allTrainedCatalanDF = pd.concat(trainedCatalanDFs)
+allTrainedCatalanDF.head()
+
+"""#Across languages t-tests
+
+##Trained Spanish vs Trained Catalan
+"""
+
+def acrossLanguagePValueEvaluation(p, combination, feat1, feat2):
+   if p < p_condition:
+      print("For", combination, ",for the feature pair", feat1, feat2, "p value is <=", str(p_condition))
+      exit(1)
+
+
+pVals_trainedSpanishVsTrainedCatalan = []
+_, p_nsyl = stats.ttest_ind(allTrainedSpanishDF['Espal_NSYL'].dropna(), allTrainedCatalanDF['CAT_SUBTLEX_NSYL'].dropna())
+acrossLanguagePValueEvaluation(p_nsyl, "trained Spanish vs trained Catalan", 'Espal_NSYL', 'CAT_SUBTLEX_NSYL')
+_, p_nlet = stats.ttest_ind(allTrainedSpanishDF['Espal_NLET'].dropna(), allTrainedCatalanDF['NIM_NLET'].dropna())
+acrossLanguagePValueEvaluation(p_nsyl, "trained Spanish vs trained Catalan", 'Espal_NLET', 'NIM_NLET')
+_, p_wordFreq = stats.ttest_ind(allTrainedSpanishDF['Coca_word Freq (Total) - Web/Dialects'].dropna(), allTrainedCatalanDF['NIM_WordFreq_Relativa'].dropna())
+acrossLanguagePValueEvaluation(p_wordFreq, "trained Spanish vs trained Catalan", 'Coca_word Freq (Total) - Web/Dialects', 'NIM_WordFreq_Relativa')
+pVals_trainedSpanishVsTrainedCatalan = [p_nsyl,p_nlet,p_wordFreq]
+
+
+"""##Trained Catalan vs Untrained Catalan"""
+
+allUntrainedDFs = pd.concat(untrainedDFs)
+allUntrainedDFs.head()
+
+pVals_trainedCatalanVsUntrainedCatalan = []
+_, p_nsyl = stats.ttest_ind(allTrainedCatalanDF['CAT_SUBTLEX_NSYL'].dropna(), allUntrainedDFs['CAT_SUBTLEX_NSYL'].dropna())
+acrossLanguagePValueEvaluation(p_nsyl, "Trained Catalan vs Untrained Catalan", 'CAT_SUBTLEX_NSYL', 'CAT_SUBTLEX_NSYL')
+_, p_nlet = stats.ttest_ind(allTrainedCatalanDF['NIM_NLET'].dropna(), allUntrainedDFs['NIM_NLET'].dropna())
+acrossLanguagePValueEvaluation(p_nlet, "Trained Catalan vs Untrained Catalan", 'NIM_NLET', 'NIM_NLET')
+
+_, p_wordFreq = stats.ttest_ind(allTrainedCatalanDF['NIM_WordFreq_Relativa'].dropna(), allUntrainedDFs['NIM_WordFreq_Relativa'].dropna())
+acrossLanguagePValueEvaluation(p_wordFreq, "Trained Catalan vs Untrained Catalan", 'NIM_WordFreq_Relativa', 'NIM_WordFreq_Relativa')
+
+pVals_trainedCatalanVsUntrainedCatalan = [p_nsyl,p_nlet,p_wordFreq]
+
+"""##Trained Spanish Vs Untrained Catalan"""
+
+pVals_trainedSpanishVsUntrainedCatalan = []
+_, p_nsyl = stats.ttest_ind(allTrainedSpanishDF['Espal_NSYL'].dropna(), allUntrainedDFs['CAT_SUBTLEX_NSYL'].dropna())
+acrossLanguagePValueEvaluation(p_nsyl, "Trained Spanish vs Untrained Catalan", 'Espal_NSYL', 'CAT_SUBTLEX_NSYL')
+
+_, p_nlet = stats.ttest_ind(allTrainedSpanishDF['Espal_NLET'].dropna(), allUntrainedDFs['NIM_NLET'].dropna())
+acrossLanguagePValueEvaluation(p_nlet, "Trained Spanish vs Untrained Catalan", 'Espal_NSYL', 'NIM_NLET')
+
+
+_, p_wordFreq = stats.ttest_ind(allTrainedSpanishDF['Coca_word Freq (Total) - Web/Dialects'].dropna(), allUntrainedDFs['NIM_WordFreq_Relativa'].dropna())
+acrossLanguagePValueEvaluation(p_wordFreq, "Trained Spanish vs Untrained Catalan", 'Coca_word Freq (Total) - Web/Dialects', 'NIM_WordFreq_Relativa')
+
+pVals_trainedSpanishVsUntrainedCatalan = [p_nsyl,p_nlet,p_wordFreq]
+
+"""##Trained Spanish Vs Untrained Spanish"""
+
+pVals_trainedSpanishVsUntrainedSpanish = []
+_, p_nsyl = stats.ttest_ind(allTrainedSpanishDF['Espal_NSYL'].dropna(), allUntrainedDFs['Espal_NSYL'].dropna())
+acrossLanguagePValueEvaluation(p_nsyl, "Trained Spanish vs Untrained Spanish", 'Espal_NSYL', 'Espal_NSYL')
+_, p_nlet = stats.ttest_ind(allTrainedSpanishDF['Espal_NLET'].dropna(), allUntrainedDFs['Espal_NLET'].dropna())
+acrossLanguagePValueEvaluation(p_nlet, "Trained Spanish vs Untrained Spanish", 'Espal_NLET', 'Espal_NLET')
+_, p_wordFreq = stats.ttest_ind(allTrainedSpanishDF['Coca_word Freq (Total) - Web/Dialects'].dropna(), allUntrainedDFs['Coca_word Freq (Total) - Web/Dialects'].dropna())
+acrossLanguagePValueEvaluation(p_wordFreq, "Trained Spanish vs Untrained Spanish", 'Coca_word Freq (Total) - Web/Dialects', 'Coca_word Freq (Total) - Web/Dialects')
+pVals_trainedSpanishVsUntrainedSpanish = [p_nsyl,p_nlet,p_wordFreq]
+
+"""##Trained Catalan Vs Untrained Spanish"""
+
+pVals_trainedCatalanVsUntrainedSpanish = []
+_, p_nsyl = stats.ttest_ind(allTrainedCatalanDF['CAT_SUBTLEX_NSYL'].dropna(), allUntrainedDFs['Espal_NSYL'].dropna())
+acrossLanguagePValueEvaluation(p_nsyl, "Trained Catalan vs Untrained Spanish", 'CAT_SUBTLEX_NSYL', 'Espal_NSYL')
+_, p_nlet = stats.ttest_ind(allTrainedCatalanDF['NIM_NLET'].dropna(), allUntrainedDFs['Espal_NLET'].dropna())
+acrossLanguagePValueEvaluation(p_nlet, "Trained Catalan vs Untrained Spanish", 'CAT_SUBTLEX_NSYL', 'Espal_NSYL')
+_, p_wordFreq = stats.ttest_ind(allTrainedCatalanDF['NIM_WordFreq_Relativa'].dropna(), allUntrainedDFs['Coca_word Freq (Total) - Web/Dialects'].dropna())
+acrossLanguagePValueEvaluation(p_wordFreq , "Trained Catalan vs Untrained Spanish", 'NIM_WordFreq_Relativa', 'Coca_word Freq (Total) - Web/Dialects')
+pVals_trainedCatalanVsUntrainedSpanish = [p_nsyl,p_nlet,p_wordFreq]
+
+"""Summarize and output"""
+
+acrossLanguageTestPVals = [pVals_trainedSpanishVsTrainedCatalan,pVals_trainedCatalanVsUntrainedCatalan,pVals_trainedSpanishVsUntrainedCatalan,pVals_trainedSpanishVsUntrainedSpanish,pVals_trainedCatalanVsUntrainedSpanish]
+
+acrossLanguageTestDFs = pd.DataFrame(acrossLanguageTestPVals, columns = ['N_SYL', 'NLET', 'WordFreq'])
+acrossLanguageTestDFs.head()
+
+descriptors = ['TrainedSpanishVsTrainedCatalan', 'TrainedCatalanVsUntrainedCatalan', 'TrainedSpanishVsUntrainedCatalan','TrainedSpanishVsUntrainedSpanish','TrainedCatalanVsUntrainedSpanish' ]
+
+acrossLanguageTestDFs.insert(0,'Descriptor',descriptors)
+acrossLanguageTestDFs.head()
+
+acrossLanguageTestDFs.to_csv(outputDirectory + 'acrossLanguageTTests.csv', index=False)
+
---
+++ setMatchingChangedWhileLoop(NF).sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 input_csv output_dir p_min"
+    exit 1
+fi
+
+inputSpreadSheet=$1
+outputDirectory=$2
+p_min=$3
+failed_runs=0
+
+while true; do
+    #python setMatchingWhileLoop.py FINAL_R01_LRT_PreTx_Probing_DataSheet.csv trialOutputLoop/ 0.01
+    #./setMatchingWhileLoop.sh FINAL_R01_LRT_PreTx_Probing_DataSheet.csv trialOutputLoop/ 0.01
+    python setMatchingChangedWhileLoop.py "$inputSpreadSheet" "$outputDirectory" "$p_min"
+    exit_code=$?
+
+    if [ $exit_code -eq 0 ]; then
+        echo "Script executed successfully."
+        break
+    else
+        ((failed_runs++))  # Increment the failed_runs count
+        echo "Generated sets didn't fulfill p value requirements. Regenerating sets..."
+    fi
+done
+
+# Print the number of failed runs at the end
+echo "Number of combinations tried out before all p values were > p_min: $failed_runs"
